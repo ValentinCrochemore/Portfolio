caffcaa77d1a360f95223b4a2c8eb2450a3667e9
Valentin Crochemore <valentin.crochemore@gmail.com>
2017-01-28 20:02:25 +0100

    Make diamonds move

--- src/components/Diamond.vue
+++ src/components/Diamond.vue
@@ -7,20 +7,29 @@
 
   export default{
     name: 'diamond',
-    data: function () {
-      return {
-        width: '100px'
-      }
-    },
     mounted: function () {
-      let size = Math.round((Math.random() * 100 + Math.random() * 100) + 40)
-      let topPosition = Math.round(Math.random() * 300)
-      let leftPosition = Math.round(Math.random() * 100)
-      this.$el.style.width = size + 'px'
-      this.$el.style.height = size + 'px'
-      this.$el.style.top = topPosition + '%'
-      this.$el.style.left = leftPosition + '%'
-      console.log('document height :', $(document).height())
+      this.randomDiamondParams()
+      $('#app').on('mousemove', this.onMouseMove)
+    },
+    methods: {
+      randomDiamondParams: function () {
+        let size = Math.round((Math.random() * 100 + Math.random() * 100) + 40)
+        let topPosition = Math.round(Math.random() * 300)
+        let leftPosition = Math.round(Math.random() * 100)
+        let diamonds = $(this.$el).siblings('.diamond')
+        diamonds.each((index, diamond) => {
+          if (diamond.offsetWidth === size) {
+            size = Math.round((Math.random() * 100 + Math.random() * 100) + 40)
+          }
+        })
+        this.$el.style.width = size + 'px'
+        this.$el.style.height = size + 'px'
+        this.$el.style.top = topPosition + '%'
+        this.$el.style.left = leftPosition + '%'
+      },
+      onMouseMove: function (e) {
+        $(this.$el).css('transform', 'rotate(45deg) translate3d(' + e.originalEvent.movementX + 'px, ' + e.originalEvent.movementY + 'px, 0)')
+      }
     }
   }
 </script>
